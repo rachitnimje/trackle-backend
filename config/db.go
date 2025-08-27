d13c155137942950f85e2430fcb67407fe6f523a
Halogen96 <rachitnimje9124@gmail.com>
2025-08-28 01:24:46 +0530

    used db_url instead

--- config/db.go
+++ config/db.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -12,17 +11,17 @@ import (
 )
 
 func ConnectDB() *gorm.DB {
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("SSL_MODE"),
-	)
+	// dsn := fmt.Sprintf(
+	// 	"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+	// 	os.Getenv("DB_HOST"),
+	// 	os.Getenv("DB_USER"),
+	// 	os.Getenv("DB_PASSWORD"),
+	// 	os.Getenv("DB_NAME"),
+	// 	os.Getenv("DB_PORT"),
+	// 	os.Getenv("SSL_MODE"),
+	// )
 
-	// dsn := os.Getenv(("DB_URL"))
+	dsn := os.Getenv(("DB_URL"))
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
